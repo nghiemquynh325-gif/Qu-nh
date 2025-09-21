964062ac547e8cfaf6784c0a2ce7482f239b6877
nghiemquynh325-gif <nghiemquynh325@gmail.com>
2025-09-21 17:28:06 +0700

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,7 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: '/resident-management-system/',
   plugins: [react()],
   server: {
     host: true, // Listen on all addresses, including localhost and network IP
